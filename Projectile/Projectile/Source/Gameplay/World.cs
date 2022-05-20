01d6f2155832e6e92f32fe59f06eaac677d3135c
Nattharika158 <105863742+Nattharika158@users.noreply.github.com>
2022-05-20 13:52:23 +0700

    Update World.cs

--- Projectile/Projectile/Source/Gameplay/World.cs
+++ Projectile/Projectile/Source/Gameplay/World.cs
@@ -13,12 +13,14 @@ namespace Projectile
     {
         public Thief thief;
         public Arrow arrow;
+        public Object item;
 
         public World()
         {
 
             thief = new Thief("thief", new Vector2(20, 400), new Vector2(40, 40));
             arrow = new Arrow("arrow", new Vector2(60, 400), new Vector2(40, 40));
+            item = new Object("Flower", new Vector2(60, 400), new Vector2(30, 30));
         }
 
         public virtual void Update()
